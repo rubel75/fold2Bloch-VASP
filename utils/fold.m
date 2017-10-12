06152089805a052cd740e95fde154586954c35a0
rubel75 <oleg.v.rubel@gmail.com>
2017-10-11 22:22:10 -0400

    Update fold.m

--- utils/fold.m
+++ utils/fold.m
@@ -2,29 +2,33 @@
 %
 % This Matlab script is designed to fold a k-path into the BZ of 
 % a supercell and produce KPOINTS file for VASP. The wavefunctions 
-% generated with VASP can be unfolded back to a desured k-path set 
+% generated with VASP can be unfolded back to a desired k-path set 
 % below.
 %
-% (c) Oleg Rubel, modified Oct 7, 2017
+% (c) Oleg Rubel, modified Oct 11, 2017
 %
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 
 clear all;
 
 %% User input
+
 kpath = [1/2 0 0; ...
          0 0 0;...
-         1/2 1/2 0];
+         1/2 1/2 0]; % desired k-path after unfolding
 npath = [10 15]; % # of points along each segment
 folds = [1 2 3]; % multiplicity used to create a supercell
 
-%% Check
+
+%% Check input
+
 if (size(kpath,1)-1 ~= length(npath))
-    error('dimensions "kpath" and "npt" do not agree')
+    error('dimensions "kpath" and "npath" do not agree')
 end
 
-%% Compute
+
+%% Compute folded k-points
+
 npt = sum(npath) - length(npath) + 1;
 kpr = zeros(npt,3);
 for i=1:size(kpath,1)-1
@@ -42,22 +46,12 @@ for i=1:size(kpath,1)-1
     end
 end
 
-figure(1);
-plot(kpr(:,1),kpr(:,2),'o')
-grid on
-pbaspect([1 1 1])
-
-ksc = zeros(size(kpr)); % allocate k-list in supercell
+ksc = zeros(size(kpr)); % allocate k-list for supercell
 for i=1:npt
     ksc(i,:) = kpr(i,:).*folds;
 end
 
-figure(2);
-plot(ksc(:,1),ksc(:,2),'o')
-grid on
-pbaspect([1 1 1])
-
-% bring the result between [-0.5, 0.5]
+% bring k-points coordinates to the range [-0.5, 0.5)
 for i=1:npt
     for j=1:3
         a = ksc(i,j);
@@ -67,21 +61,17 @@ for i=1:npt
     end
 end
 
-figure(3);
-plot(ksc(:,1),ksc(:,2),'o')
-grid on
-pbaspect([1 1 1])
 
 %% Write KPOINTS file
 
 fileID = fopen('KPOINTS','w');
-fprintf(fileID,'%34s\n','k-mesh for unfolder band structure');
+fprintf(fileID,'%34s\n','k-mesh for unfolding the band structure');
 fprintf(fileID,'%8i\n',npt);
 fprintf(fileID,'%18s\n','Reciprocal lattice');
 for i=1:npt
-    if (i ~= npt)
+    if (i ~= npt) % print with return at the end "\n"
         fprintf(fileID,'%13.10f %13.10f %13.10f  1\n',ksc(i,:));
-    else
+    else % no return for the last line
         fprintf(fileID,'%13.10f %13.10f %13.10f  1',ksc(i,:));
     end
 end
