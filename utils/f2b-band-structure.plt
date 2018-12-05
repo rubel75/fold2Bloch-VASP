97d5e18f5427ea21e98739367de6a7121910170d
Oleg Rubel <rubel@gra-login1.graham.sharcnet>
2018-12-05 18:43:00 -0500

    bitmap plot new files

---
+++ utils/f2b-band-structure.plt
@@ -0,0 +1,51 @@
+#set terminal postscript eps color enhanced font "Whitney-Book,16" size 20cm,12.0cm
+set terminal postscript eps color enhanced font "Helvetica,16" size 20cm,12cm
+
+
+set output "f2b-band-structure.eps"
+set encoding utf8 # nice minus
+set minussign # nice minus
+set multiplot layout 2,3
+
+# PLOT 1
+#set origin 0.0,0.0
+#set size 1.0,0.33
+set xlabel "Wave vector"
+set ylabel "Energy (eV)"
+set format y "%.1f" # 1 digit after period
+set border linewidth 1.0
+#set datafile separator "," # for *.csv file
+set yrange [-2.0:3.0]
+set xrange [0:0.34432]
+set cbrange [0:4]
+set xtics ("L" 0, "G" 0.15824, "X" 0.34432)
+set border lw 2 lc rgb "white"
+set xtics tc rgb "black"
+set ytics tc rgb "black"
+set cbtics tc rgb "black"
+set tics front
+set label 1 "GaP-zb" at graph 0.05,0.95 front textcolor "white"
+plot "GaP-zb-supecell-optimized.bin" binary matrix with image title "",\
+     0 title "" lc "white" lw 2 lt 2 dt 2
+
+# PLOT 2
+#set label 1 "GaP-wz (not opt.)" at graph 0.05,0.95 front textcolor "white"
+#set xrange [0:0.34095]
+#set xtics ("L" 0, "G" 0.15824, "X" 0.34095)
+#plot "GaP-wz-supecell-notOptimized.bin" binary matrix with image title "",\
+#     0 title "" lc "white" lw 2 lt 2 dt 2
+
+# PLOT 3
+#set label 1 "GaP-wz" at graph 0.05,0.95 front textcolor "white"
+#set xrange [0:0.33995]
+#set xtics ("L" 0, "G" 0.15725, "X" 0.33995)
+#plot "GaP-wz-supecell-optimized.bin" binary matrix with image title "",\
+#     0 title "" lc "white" lw 2 lt 2 dt 2
+
+# PLOT 3
+#set label 1 "GaN-wz" at graph 0.05,0.95 front textcolor "white"
+#set yrange [-2.0:5.0]
+#set xrange [0:0.41545]
+#set xtics ("L" 0, "G" 0.19297, "X" 0.41545)
+#plot "GaN-wz-supecell.bin" binary matrix with image title "",\
+#     0 title "" lc "white" lw 2 lt 2 dt 2
---
+++ utils/ubs_bmp_VASP.m
@@ -0,0 +1,198 @@
+function ubs_bmp_VASP
+% Prepare a binary file to plot the undolded band structure as a bitmap figure
+%
+% Execution:
+%    octave ubs_bmp_VASP
+%
+% Generated file:
+%    WAVECAR_spin1.f2b.bin
+%
+% Band structure plotting:
+%    gnuplot f2b-band-structure.plt
+%
+% (c) Oleg Rubel modified 05 Dec 2018
+
+%% Init. parameters
+KPATH = [0 0 1/2;...
+         0 0 0; ...
+         1/2 1/2 0]; % k-point path
+FOLDS = [1 1 6]; % multiplicity in the corresponding directions used when constructing the super-cell
+KLABEL = {'Z'; 'G'; 'L'};
+finpt = 'WAVECAR_spin1.f2b'; % input file name
+Ef = 3.237409; % Fermi energy (Ry)
+ERANGE = [Ef-2.0 Ef+3.0]; % energy range for plot (eV)
+pwr = 1/1; % power for result plotting
+         % 1 - linear scale, 1/2 - sqrt, etc.
+         % 0 - folded bands (needs wth = 0)
+nK = 280; % pixels along k
+nE =  400; % pixels along Energy axis
+sK = 0.0025; % smearing factor in k-space
+sE = 0.025; % smearing factor in energy
+G = [0.259148502 -0.149619435 -0.104836011
+0.000000000  0.299238904 -0.104836011
+0.000000000  0.000000000  0.052418005];    % Reciprocal latt. vect. from *.outputkgen
+
+%% INITIALIZATION
+[KEIG, EIG, W] = readinput(finpt); % read input data from file
+% EIG - energy eigenvalues
+% KEIG - k-list for eigenvalues
+% W - list of characters
+
+%% MAIN
+L = [];
+ENE = [];
+WGHT = [];
+%G = G'; % transpose G matrix (need for Wien2k)
+for i=1 : 3
+    G(i,:)=G(i,:)*FOLDS(i); % rescale reciprocal lattice vectors 
+end                         % from supercell to primitive cell
+dl = 0; % cumulative length of the path
+KPATH = coordTransform(KPATH,G);
+KEIG = coordTransform(KEIG,G);
+XTICKS = [0];
+for ikp = 1 : size(KPATH,1)-1
+    B = KPATH(ikp,:) - KPATH(ikp+1,:);
+    dk = sqrt(dot(B,B));
+    XTICKS = [XTICKS; XTICKS(ikp)+dk];
+    for j = 1 : length(EIG)
+        if EIG(j) > ERANGE(1) && EIG(j) < ERANGE(2) && W(j) > 0
+            dist = dp2l( KEIG(j,:) , KPATH(ikp,:) , KPATH(ikp+1,:) );
+            if dist < eps % k-point is on the path
+                A = KPATH(ikp,:) - KEIG(j,:);
+                x = dot(A,B)/dk;
+                if x >= 0  &&  x <= dk+eps % k-point is within the path range
+                    L = [L; x+dl]; % append k-point coordinate along the path
+                    ENE = [ENE; EIG(j)]; % append energy list
+                    WGHT = [WGHT; W(j)];
+                end
+            end
+        end
+    end
+    dl = dl + dk;
+end
+if isempty(L)
+    msg = ['No eigenvalues are selected for the plot. ', ...
+        'The likely reason is that the energy range is ', ...
+        'too restrictive (check ERANGE), or no k-points are located ', ...
+        'on the path selected (check KPATH)'];
+    error(msg);
+end
+
+
+WGHTRS = rescale(WGHT,pwr);
+Li = linspace(XTICKS(1),XTICKS(end),240);
+Ei = linspace(ERANGE(1)-Ef,ERANGE(2)-Ef,200);
+nLi = length(Li);
+nEi = length(Ei);
+Wi = zeros(nLi,nEi);
+ENE = ENE-Ef;
+[XTICKS(1) XTICKS(end)]
+for l = 1:length(L) % loop over all unfolded points
+    disp(100*l/length(L));
+    for i = 1:nLi
+    if abs(Li(i)-L(l)) > 3*sK
+        continue; % skip if > 3*sigma
+    endif
+        for j = 1:nEi
+            if abs(Ei(j)-ENE(l)) > 3*sE
+                continue; % skip if > 3*sigma
+            endif
+            wK = gauss(Li(i),L(l),sK);
+            wE = gauss(Ei(j),ENE(l),sE);
+            Wi(i,j) = Wi(i,j) + wK*wE*WGHTRS(l);
+        end
+    end
+end
+Wi = Wi/(gauss(0,0,sK)*gauss(0,0,sE)); % normalize weights
+save_binary_matrix([finpt,'.bin'],Li,Ei,Wi')
+XTICKS
+KLABEL
+
+%set(gca,'XTick',XTICKS);
+%set(gca,'XTickLabel',KLABEL);
+
+% -------------------------------------------------------------------------
+function RES = gauss(X,x0,s) % bound. conditions
+RES = 1/(s*sqrt(2*pi)) * exp(-((X-x0).^2)/(2*s^2));
+endfunction
+% -------------------------------------------------------------------------
+function W = coordTransform(V,G)
+% transform vector V(:,3) in G(3,3) coord. system -> W(:,3) in Cartesian coordinates
+% G vector elements are in columns!
+W = zeros(size(V));
+for i = 1:size(V,1)
+    W(i,:) = G(1,:)*V(i,1) + G(2,:)*V(i,2) + G(3,:)*V(i,3);
+end;
+end
+% -------------------------------------------------------------------------
+function WRESCL = rescale(W,pwr)
+% rescale weights using a power functio W^pwr
+WRESCL=W.^(pwr); % rescale if needed to enhance
+WRESCL = WRESCL + eps; % need eps to make plot "heapy"
+end
+% -------------------------------------------------------------------------
+function [KEIG, EIG, W] = readinput(filename)
+% read input data
+DATA = importdata(filename);
+KEIG = DATA(:,1:3);
+EIG = DATA(:,4);
+W = DATA(:,5);
+end
+% -------------------------------------------------------------------------
+function RES = dp2l(X0,X1,X2) % distance from point {X0} to line {X1}-{X2}
+% see http://mathworld.wolfram.com/Point-LineDistance3-Dimensional.html
+denom = X2 - X1;
+denomabs = sqrt(dot(denom,denom));
+if denomabs < eps
+    display(X1); display(X2);
+    error('X1 = X2');
+end;
+numer = cross( X0-X1 , X0-X2 );
+numerabs = sqrt(dot(numer,numer));
+RES = numerabs/denomabs;
+end
+% -------------------------------------------------------------------------
+function save_binary_matrix(file,x,y,M)
+% SAVE_BINARY_MATRIX save x,y,M in the matrix binary format of Gnuplot
+%   Usage: save_binary_matrix(file,x,y,M)
+%
+%   Input parameters:
+%       file    - filename of the data file
+%       x       - x axis values
+%       y       - y axis values
+%       M       - matrix data size(M)=y,x
+%
+%   SAVE_BINARY_MATRIX(file,x,y,M) saves the values of x,y and M in a binary
+%   matrix format useable by Gnuplot, see
+%   http://www.gnuplot.info/docs_4.4/gnuplot.html#x1-33700077.1.1 for details.
+%
+
+% AUTHOR: Hagen Wierstorf
+
+
+% Checking of input  parameters 
+error(nargchk(4,4,nargin));
+
+
+% Computation 
+
+% Check if the data has the right format
+[ly,lx] = size(M);
+if lx~=length(x) || ly~=length(y)
+    error('%s: size(M) has to be y,x!',upper(mfilename));
+end
+
+% Create matrix to store in the file
+MS = zeros(length(x)+1,length(y)+1);
+MS(1,1) = length(x);
+MS(1,2:end) = y;
+MS(2:end,1) = x;
+MS(2:end,2:end) = M';
+
+% Write data into the file
+fid = fopen(file,'w');
+fwrite(fid,MS,'float');
+fclose(fid);
+end
+
+end % function ubs_bmp_VASP
